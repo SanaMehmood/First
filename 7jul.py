5f49e357a6cc5ac4341ecb1f0573c65d644c0c9e
SanaMahmood <sanamahmood162@gmail.com>
2019-07-07 17:47:07 +0500

    initial commit

---
+++ 7jul.py
@@ -0,0 +1,8 @@
+user_input = int(input("Enter your Choice"))
+if user_input == 1:
+    print("English to Alie")
+    
+elif user_input == 2:
+    print ("Alie to English")
+else:
+     print ("Invalid")
